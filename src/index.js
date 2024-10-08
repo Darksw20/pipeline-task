faf35bc54bd58d8702bcd33894083fe2d486f9ea
Aldayr02 <id724577@iteso.mx>
2024-10-07 20:12:06 -0600

    feat: create basic folders and files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-const express = require("express");
-const { config } = require("dotenv");
+const express = require('express');
+const { config } = require('dotenv');
 
 // init dotenv config
 config();
 
 // variables
 const app = express();
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
 
 // test get
-app.get("/", (req, res) => res.json({ msg: "Hello" }));
+app.get('/', (req, res) => res.json({ msg: 'Hello' }));
 
 // listen
-app.listen(process.env.PORT || 3000, () => {
-  console.log("App running");
+app.listen(port, () => {
+  console.log(`App running in port ${port}`);
 });
