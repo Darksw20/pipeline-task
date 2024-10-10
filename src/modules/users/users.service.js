27d3436887e41caab30dc05a78a4b3cea64355c8
Salvador025 <jose.mariscal@iteso.mx>
2024-10-09 19:16:45 -0600

    users-service funciones para la bd

--- src/modules/users/users.service.js
+++ src/modules/users/users.service.js
@@ -1,8 +1,49 @@
 /** Handle logic and DB connnection */
 
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'username',
+  database: 'database_name',
+  password: 'password',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
 class UsersService {
   getHello() {
     return { message: "Hello" };
+  }
+
+  async createUser(username, password, email) {
+    const query = 'INSERT INTO users (username, password, email) VALUES (?, ?, ?)';
+    try {
+      const [result] = await pool.query(query, [username, password, email]);
+      const userId = result.insertId;
+      return this.findUserById(userId);
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+
+  async findUserByUsername(username) {
+    const query = 'SELECT * FROM users WHERE username = ?';
+    try {
+      const [rows] = await pool.query(query, [username]);
+      return rows[0];
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+
+  async findUserById(userId) {
+    const query = 'SELECT * FROM users WHERE id = ?';
+    try {
+      const [rows] = await pool.query(query, [userId]);
+      return rows[0];
+    } catch (err) {
+      throw new Error(err.message);
+    }
   }
 }
 
