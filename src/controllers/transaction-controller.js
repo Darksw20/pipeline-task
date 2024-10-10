0a36dea1d4c72d3b1a099e1a39d20983a85be63d
VMLAP\VMLap <victor.telles@iteso.mx>
2024-10-09 19:19:54 -0600

    Implementacion de la funcionalidad de GET

--- src/controllers/transaction-controller.js
+++ src/controllers/transaction-controller.js
@@ -5,7 +5,29 @@ class TransactionController {
   //     res.send(`Controller funcionando`);
   //   }
 
-  getTransactions(req, res) {}
+  //Obtener transacciones
+  getTransactions = (req, res) => {
+    const { user_id } = req.query;
+
+    if (!user_id) {
+      return res.status(400).json({ message: 'User ID es requerido' });
+    }
+
+    TransactionModel.findAll({
+      where: { user_id: user_id},
+      attributes: ['currency', 'amount', 'price_per_unit', 'total_price'],
+    })
+      .then((transactions) => {
+        if (transactions.length === 0) {
+          return res.status(404).json({ message: 'No se han encontrado transacciones para este usuario'})
+        }
+        res.status(200).json(transactions);
+      })
+      .catch((error) => {
+        console.error('Error al obtener transacciones:', error);
+        res.status(500).json({ error: 'Error al obtener las transacciones: ' + error.message });
+      });
+  };
 
   createTransaction = (req, res) => {
     const { user_id, currency, amount, price_per_unit, total_price } = req.body;
