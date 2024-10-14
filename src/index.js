53870855f120f877f4a6e5962ae84410dd98bd1b
joseCarlosAc <jose.acevesgon@gmail.com>
2024-10-13 23:26:13 -0600

    Add endpoint for transactions page

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
+app.get("/transactions", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public", "transactions.html"));
+});
+
 // listen
 app.listen(process.env.PORT || 3000, () => {
 	console.log("App running");
